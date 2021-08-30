51a9c593a3c0b257a7f3450f0435cbc12f732c07
Amino <28660350+Aminoa@users.noreply.github.com>
2021-08-29 20:29:11 -0400

    Adds begin files
    Has the starter create-react-app files, modified for initial version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function WriteLink(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="WriteLink">
+      <li> <a href={props.location}> {props.name} </a> <span class="material-icons">link</span> </li>
     </div>
   );
 }
+
+function App() {
+
+  return (
+    <div className="App">
+      <h2 id="title">Tandon Syllabi</h2>
+      <h2 id="courses">Courses:</h2>
+      <ul>
+        <WriteLink location="/syllabi/chemistry/CM-UY - 1003.pdf" name="General Chemistry for Engineers"> </WriteLink>
+        <WriteLink location="/syllabi/cs/CS-UY - 1122.pdf" name="Introduction to Computer Science"> </WriteLink> 
+        <WriteLink location="/syllabi/cs/CS-UY - 1134.pdf" name="Data Structures and Algorithms"> </WriteLink> 
+        <WriteLink location="/syllabi/cs/CS-UY - 2124.pdf" name="Object Oriented Programming"> </WriteLink> 
+        <WriteLink location="https://sarathermer.com/eg-1001-syllabus" name="Engineering and Technology Forum (EG-1001)"> </WriteLink> 
+        <WriteLink location="/syllabi/engineering/EG-UY - 1003.pdf" name="Introduction to Engineering and Design (EG-1003)"> </WriteLink> 
+        <WriteLink location="/syllabi/expos/EXPOS-UA - 1.pdf" name="Writing the Essay"> </WriteLink> 
+        <WriteLink location="/syllabi/expos/EXPOS-UA - 24.pdf" name="Advanced Writing for Engineers: Entrepreneurship"> </WriteLink> 
+        <WriteLink location="/syllabi/math/MA-UY - 2034.pdf" name="Linear Algebra and Differential Equations"> </WriteLink> 
+        <WriteLink location="/syllabi/math/MA-UY - 2214.pdf" name="Data Analysis"> </WriteLink> 
+        <WriteLink location="/syllabi/math/MA-UY - 2314.pdf" name="Discrete Mathematics"> </WriteLink> 
+        <WriteLink location="/syllabi/physics/PH-UY - 1013.pdf" name="Mechanics"> </WriteLink> 
+      </ul>
+    </div>
+  );
+}
+
+
 
 export default App;
